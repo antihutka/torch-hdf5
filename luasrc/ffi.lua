36dc48162ff5024faa6848ef3d068fccf025e619
Marcel Samyn <dev@mrcl.io>
2018-02-17 10:28:07 +0100

    Make compatible with HDF5 1.10

--- luasrc/ffi.lua
+++ luasrc/ffi.lua
@@ -67,9 +67,9 @@ local function checkHDF5Version()
     local min = ffi.new('unsigned int[1]')
     local rel = ffi.new('unsigned int[1]')
     hdf5.C.H5get_libversion(maj, min, rel)
-    if maj[0] ~= 1 or min[0] ~= 8 then
-        error("Unsupported HDF5 version: " .. maj[0] .. "." .. min[0] .. "." .. rel[0])
-    end
+    -- if maj[0] ~= 1 or min[0] ~= 8 then
+    --     error("Unsupported HDF5 version: " .. maj[0] .. "." .. min[0] .. "." .. rel[0])
+    -- end
     hdf5.version = {tonumber(maj[0]), tonumber(min[0]), tonumber(rel[0])}
     -- This is disabled as it's a bit too specific
     -- hdf5.C.H5check_version(1, 8, 12)
